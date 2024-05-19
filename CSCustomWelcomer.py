3cde639766826b7126bad13c998b8da0741f8c61
DeltaGamesYT <127041376+DeltaGamesYT@users.noreply.github.com>
2024-05-19 20:53:50 -0300

    Embed Preview 1.1 > CSCustomWelcomer.py

--- CSCustomWelcomer.py
+++ CSCustomWelcomer.py
@@ -8,6 +8,8 @@ bot_token = "YOUR_TOKEN_GOES_HERE" # string
 welcome_id = 0 # integer
 # Leave messages channel's ID / ID del canal de mensajes Leave
 leave_id = 0 # integer
+# Message Type / Tipo de Mensaje
+mtype = "text" # string; <text:embed>
 # Messages / Mensajes
 # Welcome
 welcome_msg = "Hola [member.mention], bienvenido a **[guild.name]**!"
@@ -27,26 +29,38 @@ bot = commands.Bot(command_prefix='cscw!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print("CSCustomWelcomer v1.0.1\n\nReleased: 19/05/2024\n\nSource on https://github.com/deltagamesyt/cs-custom-welcomer")
+    print("CSCustomWelcomer v1.1.0\n\nReleased: 19/05/2024\n\nSource on https://github.com/deltagamesyt/cs-custom-welcomer")
     print(f"Cliente conectado: {bot.user.name}")
     print(f"Connected client: {bot.user.name}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='member joins'))
 @bot.event
 async def on_member_join(member):
     server_name = member.guild.name
-    if welcome_id:
+    if welcome_id and mtype.lower() == "text":
         welcomer_channel = member.guild.get_channel(welcome_id)
         message = welcome_msg.replace("[member.mention]", member.mention)
         message = message.replace("[guild.name]", server_name)
         await welcomer_channel.send(message)
+    elif welcome_id and mtype.lower() == "embed":
+        welcomer_channel = member.guild.get_channel(welcome_id)
+        message = welcome_msg.replace("[member.mention]", member.mention)
+        message = message.replace("[guild.name]", server_name)
+        embed = discord.Embed(title=member.guild.name, color=discord.Colour.from_str("0x4488ee"), description=message)
+        await welcomer_channel.send(embed=embed)
 @bot.event
 async def on_member_remove(member):
     server_name = member.guild.name
-    if leave_id:
+    if leave_id and mtype.lower() == "text":
         leave_channel = member.guild.get_channel(leave_id)
         message = leave_msg.replace("[member.name]", member.name)
         message = message.replace("[guild.name]", server_name)
         await leave_channel.send(message)
+    elif welcome_id and mtype.lower() == "embed":
+        leave_channel = member.guild.get_channel(welcome_id)
+        message = leave_msg.replace("[member.name]", member.name)
+        message = message.replace("[guild.name]", server_name)
+        embed = discord.Embed(title=member.guild.name, color=discord.Colour.from_str("0x4488ee"), description=message)
+        await leave_channel.send(embed=embed)
 @bot.command()
 async def test(ctx):
     await ctx.send("I'm Working!\n¡Estoy funcionando!")
