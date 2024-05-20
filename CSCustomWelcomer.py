b9567774c5c9e2ce82ae204245c2d98fbb43635d
DeltaGamesYT <127041376+DeltaGamesYT@users.noreply.github.com>
2024-05-20 15:13:49 -0300

    1.2.0 > CSCustomWelcomer.py

--- CSCustomWelcomer.py
+++ CSCustomWelcomer.py
@@ -15,6 +15,12 @@ mtype = "text" # string; <text:embed>
 welcome_msg = "Hola [member.mention], bienvenido a **[guild.name]**!"
 # Leave
 leave_msg = "**[member.name]** salio de **[guild.name]**"
+# Embed configuration / Configuracion de embed
+# Only modify if you are using embed mode / Solo modificar si estas usando el modo embed
+# Embed Title / Titulo del Embed
+embed_title = "[guild.name]"
+# Embed Colour / Color del Embed
+embed_color = "#4488ee" # String: Format #RRGGBB <HEX>
 
 
 # BOT CODE        CODIGO DEL BOT
@@ -25,11 +31,24 @@ intents.messages = True
 intents.message_content = True
 intents.typing = True
 intents.presences = True
+
+if mtype.lower() == "embed":
+    if len(embed_color) == 7:
+        for char in embed_color:
+            if char == "#":
+                continue
+            if not char.lower() in "0123456789abcdef":
+                print("\n\nERROR: Invalid embed color: " + str(embed_color) + ". Use #rrggbb format.")
+                exit()
+    else:
+        print("\n\nERROR: Invalid embed color: " + str(embed_color) + ". Use #rrggbb format.")
+        exit()
+
 bot = commands.Bot(command_prefix='cscw!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print("CSCustomWelcomer v1.1.0\n\nReleased: 19/05/2024\n\nSource on https://github.com/deltagamesyt/cs-custom-welcomer")
+    print("CSCustomWelcomer v1.2.0\n\nReleased: 20/05/2024\n\nSource on https://github.com/deltagamesyt/cs-custom-welcomer")
     print(f"Cliente conectado: {bot.user.name}")
     print(f"Connected client: {bot.user.name}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='member joins'))
@@ -45,7 +64,8 @@ async def on_member_join(member):
         welcomer_channel = member.guild.get_channel(welcome_id)
         message = welcome_msg.replace("[member.mention]", member.mention)
         message = message.replace("[guild.name]", server_name)
-        embed = discord.Embed(title=member.guild.name, color=discord.Colour.from_str("0x4488ee"), description=message)
+        title = embed_title.replace("[guild.name]", server_name)
+        embed = discord.Embed(title=title, color=discord.Colour.from_str(embed_color), description=message)
         await welcomer_channel.send(embed=embed)
 @bot.event
 async def on_member_remove(member):
@@ -59,7 +79,8 @@ async def on_member_remove(member):
         leave_channel = member.guild.get_channel(welcome_id)
         message = leave_msg.replace("[member.name]", member.name)
         message = message.replace("[guild.name]", server_name)
-        embed = discord.Embed(title=member.guild.name, color=discord.Colour.from_str("0x4488ee"), description=message)
+        title = embed_title.replace("[guild.name]", server_name)
+        embed = discord.Embed(title=title, color=discord.Colour.from_str(embed_color), description=message)
         await leave_channel.send(embed=embed)
 @bot.command()
 async def test(ctx):
